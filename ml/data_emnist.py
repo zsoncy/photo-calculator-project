a648979c734acf807bdb84c2e2b2fa7eb2bf77d0
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2026-01-27 17:03:24 +0100

    Refactor the data input files

--- ml/data_emnist.py
+++ ml/data_emnist.py
@@ -1,17 +1,15 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
 
-# pick a split: 'byclass' or 'balanced'
+
 def load_emnist_byclass(batch_size=128):
-    ds_train, ds_test = tfds.load("emnist/byclass", split=["train", "test"], as_supervised=True)
-    # EMNIST images are 28x28 grayscale; normalize to [0,1], ensure shape (28,28,1)
+    ds_train, ds_test = tfds.load("emnist/digits", split=["train", "test"], as_supervised=True)
     def prep(x, y):
         x = tf.cast(x, tf.float32) / 255.0
-        x = tf.expand_dims(x, -1)  # (H,W,1)
-        # EMNIST is rotated/transposed; fix orientation (official quirk)
-        x = tf.transpose(x, perm=[1, 0, 2])     # swap axes
-        x = tf.image.flip_left_right(x)         # horizontal flip
+        x = tf.transpose(x, perm=[1, 0, 2])
+        x = tf.image.flip_left_right(x)
         return x, y
-    ds_train = ds_train.map(prep, num_parallel_calls=tf.data.AUTOTUNE).shuffle(10000).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    ds_train = ds_train.map(prep, num_parallel_calls=tf.data.AUTOTUNE).shuffle(10000).batch(batch_size).prefetch(
+        tf.data.AUTOTUNE)
     ds_test = ds_test.map(prep, num_parallel_calls=tf.data.AUTOTUNE).batch(batch_size).prefetch(tf.data.AUTOTUNE)
     return ds_train, ds_test
--- ml/data_symbols.py
+++ ml/data_symbols.py
@@ -8,10 +8,15 @@ def load_symbols_from_folders(root="data/symbols", img_size=(28,28), batch_size=
         root, labels="inferred", label_mode="int", color_mode="grayscale",
         image_size=img_size, batch_size=batch_size, shuffle=True
     )
+
+    class_names = ds.class_names  # folder order
+
     # Normalize & ensure (28,28,1)
     def prep(x, y):
         x = tf.cast(x, tf.float32) / 255.0
+        y = tf.cast(y, tf.int64)
         return x, y
+
+
     ds = ds.map(prep, num_parallel_calls=tf.data.AUTOTUNE)
-    class_names = ds.class_names  # folder order
     return ds, class_names
